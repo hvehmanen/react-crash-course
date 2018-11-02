6751b48247e79319d55621242a18fc87989db6bf
Henri Vehmanen <henri.vehmanen@gmail.com>
2018-11-02 11:56:00 +0200

    Counter implementation with event handler binding using a constructor.

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,8 +1,42 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
+
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleIncrement() {
+    console.log("Increment clicked", this);
+  }
+
   render() {
-    return <h1>Hello world</h1>;
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
