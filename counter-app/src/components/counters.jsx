7dc1279186e2e43e2bf088eb22dd0652bf5d31de
Henri Vehmanen <henri.vehmanen@gmail.com>
2018-11-05 17:00:15 +0200

    Added reset button. Increment handling moved to parent (single source of truth). Weird spreading stuff.

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -6,9 +6,25 @@ class Counters extends Component {
     counters: [
       { id: 1, value: 0 },
       { id: 2, value: 0 },
-      { id: 3, value: 0 },
+      { id: 3, value: 2 },
       { id: 4, value: 0 }
     ]
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
   };
 
   handleDelete = counterId => {
@@ -19,12 +35,18 @@ class Counters extends Component {
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map(counter => (
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
-            value={counter.value}
-            id={counter.id}
+            onIncrement={this.handleIncrement}
+            counter={counter}
           />
         ))}
       </div>
