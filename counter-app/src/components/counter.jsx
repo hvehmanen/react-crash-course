233f6fa344749b41bcdc14aa2c4c39fc9718a870
Henri Vehmanen <henri.vehmanen@gmail.com>
2018-11-08 17:14:37 +0200

    More tests for the Counter component (zero handling, button clicks)

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -26,13 +26,13 @@ class Counter extends Component {
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.props.onIncrement(this.props.counter)}
-          className="btn btn-secondary btn-sm"
+          className="btn btn-secondary btn-sm increment"
         >
           Increment
         </button>
         <button
           onClick={() => this.props.onDelete(this.props.counter.id)}
-          className="btn btn-danger btn-sm m-2"
+          className="btn btn-danger btn-sm m-2 delete"
         >
           Delete
         </button>
--- counter-app/src/components/counter.test.js
+++ counter-app/src/components/counter.test.js
@@ -5,8 +5,35 @@ import Adapter from "enzyme-adapter-react-16";
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("<Counter />", () => {
-  it("renders badge with count", () => {
+  it("renders a badge with the correct count", () => {
     const counter = shallow(<Counter counter={{ id: 1, value: 2 }} />);
-    expect(counter.find("span.badge").length).toEqual(1);
+    const element = counter.find("span.badge");
+    expect(element.length).toEqual(1);
+    expect(element.text()).toEqual("2");
+  });
+
+  it("renders a badge with the text 'Zero' if the amount is 0", () => {
+    const counter = shallow(<Counter counter={{ id: 1, value: 0 }} />);
+    const element = counter.find("span.badge");
+    expect(element.length).toEqual(1);
+    expect(element.text()).toEqual("Zero");
+  });
+
+  it("calls the onIncrement method when increment button is called", () => {
+    const fakeIncrement = jest.fn();
+    const counter = shallow(
+      <Counter counter={{ id: 1, value: 2 }} onIncrement={fakeIncrement} />
+    );
+    counter.find("button.increment").simulate("click");
+    expect(fakeIncrement).toHaveBeenCalled();
+  });
+
+  it("calls the onDelete method when delete button is called", () => {
+    const fakeDelete = jest.fn();
+    const counter = shallow(
+      <Counter counter={{ id: 1, value: 2 }} onDelete={fakeDelete} />
+    );
+    counter.find("button.delete").simulate("click");
+    expect(fakeDelete).toHaveBeenCalled();
   });
 });
